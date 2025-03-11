9351ec82cf68acefb4112d8f23041d522fe59c15
FojleTuhin <fojletuhin@gmail.com>
2025-03-11 19:03:04 +0600

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken')
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "https://parcelpioneer.netlify.app"]
+  origin: ["*"]
 }))
 
 
